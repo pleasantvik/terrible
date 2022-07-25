05fababdb5cc68f6c86a2f1d5bdc32016b235493
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-07-25 14:10:15 +0100

    edit

--- app.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector(".heading");
-
-const toggleBtn = document.querySelector(".btn-mobile-nav");
-
-// MOBILE NAVIGATION
-toggleBtn.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
-});
